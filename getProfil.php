f62a03a45342a40d294e7eccba49ec554cbc15e1
Nesrine <nesrine.zegai.simplon@gmail.com>
2022-02-24 13:13:30 +0100

    git commit-m

--- getProfil.php
+++ getProfil.php
@@ -2,7 +2,7 @@
 /*
 // api_key=RGAPI-4b7d384d-0b8e-4d02-a9b4-b758c856b7e7
 */
-$response = file_get_contents("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/".$_GET['name']."?api_key=RGAPI-4b7d384d-0b8e-4d02-a9b4-b758c856b7e7");
+$response = file_get_contents("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/".$_GET['name']."?api_key=RGAPI-b28b4728-5bfc-425c-9dc0-d729cb403bb1");
 
 echo $response;
 ?>
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-$.get("./getProfil.php?name=technet")
+$.get("./getProfil.php?name=senbonzakura38")
 .done( function(data){
 	profil = jQuery.parseJSON(data);
-	$( "#result" ).text(profil.name );
+	$( "#ddragon" ).text(profil.name );
+	$("#icon").attr("src", "https://ddragon.leagueoflegends.com/cdn/12.4.1/img/profileicon/"+profil.profileIconId+".png");
+
+	//<if ("#icon ="ddragon.png.") {src ddragon.png};
 	console.log(profil);
 	console.log(profil.name);
 });
