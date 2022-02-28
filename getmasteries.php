427c134d3cbce44b75edbef52467c08554477c3f
Nesrine <nesrine.zegai.simplon@gmail.com>
2022-02-28 15:15:57 +0100

    getmasteries.php

--- getmasteries.php
+++ getmasteries.php
@@ -3,8 +3,9 @@
 	$response = @file_get_contents("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/".$_GET['name']."?api_key=RGAPI-f20459c0-0e21-40cf-ba21-586d1160615d");
 
 if ($response === FALSE){
-	$response = '{"id": "none","accountId": "none","puuid": "none","name": "inconnu","profileIconId": 0,"revisionDate": 0, "summonerLevel": 0}';
+	$response = '{"championId": 0,"championLevel": 0,"championPoints": 0,"lastPlayTime": 0,"championPointsSinceLastLevel": 0,"championPointsUntilNextLevel": 0,"chestGranted": false,"tokensEarned": 0,"summonerId": "0"}';
 }
+
 
 echo $response;
 
